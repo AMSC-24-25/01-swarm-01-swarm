4694cf9ba6bda696bc47442539dd8eff83fa9e11
poliReus <fabio11.rossi@mail.polimi.it>
2024-12-12 16:27:22 +0000

    naive implementation of particle swarm optimization Co-authored-by: TMarche37 <TMarche37@users.noreply.github.com> Co-authored-by: BenedettaMussini <BenedettaMussini@users.noreply.github.com> Co-authored-by: LorenzoBoretti <LorenzoBoretti@users.noreply.github.com> Co-authored-by: michfiora<michfiora@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -17,6 +17,12 @@ Coordinate Particle::globalBest=Coordinate();
   return val;
 }*/
 
+double rastrigin(Coordinate x) {
+    //return the rastrigin function with minimum at 3,3
+    return  12.3 + x.getX()*x.getX() + x.getY()*x.getY() - 10*(cos(2*M_PI*x.getX()) + cos(2*M_PI*x.getY()));
+    
+}
+
 
 void updates(float inertia, float cognitive, float social, vector<Particle> &particles) {
     for (int i = 0; i < N_PARTICLES; i++) {
@@ -28,6 +34,14 @@ void updates(float inertia, float cognitive, float social, vector<Particle> &par
         double x = particles[i].getPosition().getX() + vx;
         double y = particles[i].getPosition().getY() + vy;
         particles[i].updatePosition(x, y);
+        //update personal best
+        if (rastrigin(particles[i].getPosition()) < rastrigin(particles[i].getPersonalBest())) {
+            particles[i].updatePersonalBest(particles[i].getPosition().getX(), particles[i].getPosition().getY());
+        }
+        //update global best
+        if (rastrigin(particles[i].getPosition()) < rastrigin(particles[i].getGlobalBest())) {
+            particles[i].getGlobalBest().setCoordinate(particles[i].getPosition().getX(), particles[i].getPosition().getY());
+        }
     }
 
 }
@@ -41,8 +55,19 @@ int main() {
     vector<Particle> particles(N_PARTICLES);
     for (int i = 0; i < N_PARTICLES; i++) {
         particles[i] = Particle(randomGenerator(seed), randomGenerator(seed), randomGenerator(seed), randomGenerator(seed));
+        particles[i].updatePersonalBest(particles[i].getPosition().getX(), particles[i].getPosition().getY());
+        //set the global best to the first particle
+        if (i == 0) {
+            particles[i].getGlobalBest().setCoordinate(particles[i].getPosition().getX(), particles[i].getPosition().getY());
+        }
+        else{
+            //update the global best if the particle is better
+            if (rastrigin(particles[i].getPosition()) < rastrigin(particles[i].getGlobalBest())) {
+                particles[i].getGlobalBest().setCoordinate(particles[i].getPosition().getX(), particles[i].getPosition().getY());
+            }
+        }
         //print the particle position
-        cout << "Particle " << i << " position: " << particles[i].getPosition().getX() << " " << particles[i].getPosition().getY() << endl;
+        //cout << "Particle " << i << " position: " << particles[i].getPosition().getX() << " " << particles[i].getPosition().getY() << endl;
     }
     float inertia = 0.5;
     float cognitive = 0.5;
@@ -50,9 +75,10 @@ int main() {
     for(int i = 0; i < 100; i++){
         updates(inertia, cognitive, social, particles);
     }
-    for (int i = 0; i < N_PARTICLES; i++) {
+    cout << "Global best: " << rastrigin(particles[0].getGlobalBest()) <<endl;
+    /*for (int i = 0; i < N_PARTICLES; i++) {
         //print the particle position
         cout << "Particle " << i << " position: " << particles[i].getPosition().getX() << " " << particles[i].getPosition().getY() << endl;
-    }
+    }*/
     return 0;
 }
