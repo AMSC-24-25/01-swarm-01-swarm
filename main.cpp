23d8082b01b6ef92d8f874fe54effe91e7392c80
poliReus <fabio11.rossi@mail.polimi.it>
2024-12-12 14:15:35 +0000

    inclusion of coordinate.hpp in particle.hpp and particle.hpp in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-
+#include <particle.hpp>
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    
     return 0;
 }
--- particle.hpp
+++ particle.hpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <coordinate.hpp>
 
 class Particle {
-    
+    private Coordinate position;
+    private double velocity;
+    private Coordinate personalBest;
 }
