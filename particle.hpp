92a0963ac35cf8e1713d4567b0e8519df7065a58
poliReus <fabio11.rossi@mail.polimi.it>
2024-12-12 14:29:23 +0000

    adding the constructor to Particle class and some simple methods. Co-authored-by: TMarche37 <TMarche37@users.noreply.github.com> Co-authored-by: BenedettaMussini <BenedettaMussini@users.noreply.github.com> Co-authored-by: LorenzoBoretti <LorenzoBoretti@users.noreply.github.com>

--- particle.hpp
+++ particle.hpp
@@ -5,6 +5,23 @@
 
 class Particle {
     private Coordinate position;
-    private double velocity;
-    private Coordinate personalBest;
+    private Coordinate velocity;
+    private double personalBest;
+    private static Coordinate globalBest;
+
+    public Particle(double x, double y, double vx, double vy){
+        position.setCoordinate(x, y);
+        velocity.setCoordinate(vx, vy);
+        personalBest = 0;
+    }
+
+    public void updatePosition(){
+        position.setCoordinate();
+    }
+    public void updateVelocity(){
+        velocity.setCoordinate();
+    }
+    public void updatePersonalBest(double newBest){
+        personalBest = newBest;
+    }
 }
