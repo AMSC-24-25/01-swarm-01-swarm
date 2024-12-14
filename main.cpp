0fb9eafecc56602b9722f7e2efc7d320e964e92d
poliReus <fabio11.rossi@mail.polimi.it>
2024-12-14 10:27:44 +0000

    Tested PSO on Baele function, print solution in a better format Co-authored-by: TMarche37 <TMarche37@users.noreply.github.com> Co-authored-by: BenedettaMussini <BenedettaMussini@users.noreply.github.com> Co-authored-by: LorenzoBoretti <LorenzoBoretti@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -7,39 +7,38 @@
 #include <sstream>
 using namespace std;
 
-Coordinate Particle::globalBest=Coordinate();
+Coordinate Particle::globalBest=Coordinate(0,0);
 
-#define N_PARTICLES 25
-#define LOWER_BOUND -300
-#define UPPER_BOUND 300
-#define N_ITERATIONS 25
+#define N_PARTICLES 100
+#define LOWER_BOUND -4.5
+#define UPPER_BOUND 4.5
+#define N_ITERATIONS 15
 
-/*double clamp(double val) {
-  if (val < LOWER_BOUND) return LOWER_BOUND;
-  if (val > UPPER_BOUND) return UPPER_BOUND;
+double clamp(double val) {
+  if (val < -1.0) return -1.0;
+  if (val > 1.0) return 1.0;
   return val;
-}*/
+}
 
-enum FunctionType { RASTRIGIN, SPHERE, ROSENBROCK , STRANGE};
+enum FunctionType { RASTRIGIN, SPHERE, ROSENBROCK ,BAELE, STRANGE};
 
-FunctionType currentFunction = SPHERE;
+FunctionType currentFunction = BAELE;
 
 double rastrigin(Coordinate x) {
     //return the rastrigin function value
     return 20 + pow(x.getX(), 2) + pow(x.getY(), 2) - 10 * (cos(2 * M_PI * x.getX()) + cos(2 * M_PI * x.getY()));
     
 }
-
 double strangeFunction(Coordinate x) {
-    double xVal = x.getX();
-    double yVal = x.getY();
+    //return the himmelblau function value
+    return pow(pow(x.getX(), 2) + x.getY() - 11,2)+ pow(pow(x.getY(), 2) + x.getX() - 7,2);
+    
+}
 
-    // Termini con valori ridotti
-    double term1 = sin(xVal) * cos(yVal) * exp(-abs(sqrt(xVal * xVal + yVal * yVal) / M_PI));
-    double term2 = 5 * pow(sin(xVal * yVal), 2);
-
+double baele(Coordinate x) {
+  //return the beale function value
     // Combina i termini e riduce l'ampiezza
-    return -fabs(term1) + term2 - 0.5 * (xVal * xVal + yVal * yVal);
+    return (1.5-x.getX()+x.getX()*x.getY())*(1.5-x.getX()+x.getX()*x.getY())+(2.25-x.getX()+x.getX()*x.getY()*x.getY())*(2.25-x.getX()+x.getX()*x.getY()*x.getY())+(2.625-x.getX()+x.getX()*x.getY()*x.getY()*x.getY())*(2.625-x.getX()+x.getX()*x.getY()*x.getY()*x.getY());
 }
 
 
@@ -55,6 +54,8 @@ double evaluateFunction(Coordinate x) {
     switch (currentFunction) {
         case RASTRIGIN:
             return rastrigin(x);
+        case BAELE:
+            return baele(x);
         case SPHERE:
             return sphere(x);
         case ROSENBROCK:
@@ -78,8 +79,8 @@ void saveGridData() {
         return;
     }
     file << "x,y,z\n";  // Intestazione CSV
-    for (double x = LOWER_BOUND; x <= UPPER_BOUND; x += 3) {
-        for (double y = LOWER_BOUND; y <= UPPER_BOUND; y += 3) {
+    for (double x = LOWER_BOUND; x <= UPPER_BOUND; x += 0.1) {
+        for (double y = LOWER_BOUND; y <= UPPER_BOUND; y += 0.1) {
             double z = evaluateFunction(Coordinate(x, y));
             file << x << "," << y << "," << z << "\n";
         }
@@ -108,8 +109,10 @@ void saveParticleData(const vector<Particle>& particles, int iteration) {
 void updates(float inertia, float cognitive, float social, vector<Particle> &particles) {
     for (int i = 0; i < N_PARTICLES; i++) {
         //update velocity
-        double vx = inertia * particles[i].getVelocity().getX() + cognitive * (particles[i].getPersonalBest().getX() - particles[i].getPosition().getX()) + social * (particles[i].getGlobalBest().getX() - particles[i].getPosition().getX());
-        double vy = inertia * particles[i].getVelocity().getY() + cognitive * (particles[i].getPersonalBest().getY() - particles[i].getPosition().getY()) + social * (particles[i].getGlobalBest().getY() - particles[i].getPosition().getY());
+        double vx = inertia * particles[i].getVelocity().getX() + cognitive * (particles[i].getPersonalBest().getX() - particles[i].getPosition().getX()) + social * (Particle::getGlobalBest().getX() - particles[i].getPosition().getX());
+        double vy = inertia * particles[i].getVelocity().getY() + cognitive * (particles[i].getPersonalBest().getY() - particles[i].getPosition().getY()) + social * (Particle::getGlobalBest().getY() - particles[i].getPosition().getY());
+        vx = clamp(vx);
+        vy = clamp(vy);
         particles[i].updateVelocity(vx, vy);
         //update position
         double x = particles[i].getPosition().getX() + vx;
@@ -120,8 +123,8 @@ void updates(float inertia, float cognitive, float social, vector<Particle> &par
             particles[i].updatePersonalBest(particles[i].getPosition().getX(), particles[i].getPosition().getY());
         }
         //update global best
-        if (evaluateFunction(particles[i].getPosition()) < evaluateFunction(particles[i].getGlobalBest())) {
-            particles[i].getGlobalBest().setCoordinate(particles[i].getPosition().getX(), particles[i].getPosition().getY());
+        if (evaluateFunction(particles[i].getPosition()) < evaluateFunction(Particle::getGlobalBest())) {
+            Particle::updateGlobalBest(particles[i].getPosition().getX(), particles[i].getPosition().getY());
         }
     }
 
@@ -139,12 +142,12 @@ int main() {
         particles[i].updatePersonalBest(particles[i].getPosition().getX(), particles[i].getPosition().getY());
         //set the global best to the first particle
         if (i == 0) {
-            particles[i].getGlobalBest().setCoordinate(particles[i].getPosition().getX(), particles[i].getPosition().getY());
+            Particle::updateGlobalBest(particles[i].getPosition().getX(), particles[i].getPosition().getY());
         }
         else{
             //update the global best if the particle is better
-            if (evaluateFunction(particles[i].getPosition()) < evaluateFunction(particles[i].getGlobalBest())) {
-                particles[i].getGlobalBest().setCoordinate(particles[i].getPosition().getX(), particles[i].getPosition().getY());
+            if (evaluateFunction(particles[i].getPosition()) < evaluateFunction(Particle::getGlobalBest())) {
+                Particle::updateGlobalBest(particles[i].getPosition().getX(), particles[i].getPosition().getY());
             }
         }
         //print the particle position
@@ -152,13 +155,16 @@ int main() {
     }
     float inertia = 0.5;
     float cognitive = 0.5;
-    float social = 0.5;
+    float social = 1.5;
     saveGridData();
     for(int i = 0; i < N_ITERATIONS; i++){
         updates(inertia, cognitive, social, particles);
         saveParticleData(particles, i);
     }
-    cout << "Global best: " << evaluateFunction(particles[0].getGlobalBest()) <<endl;
+    cout << "Final Global Best: (" 
+     << Particle::getGlobalBest().getX() << ", " 
+     << Particle::getGlobalBest().getY() << ") Value: " 
+     << evaluateFunction(Particle::getGlobalBest()) << endl;
     /*for (int i = 0; i < N_PARTICLES; i++) {
         //print the particle position
         cout << "Particle " << i << " position: " << particles[i].getPosition().getX() << " " << particles[i].getPosition().getY() << endl;
