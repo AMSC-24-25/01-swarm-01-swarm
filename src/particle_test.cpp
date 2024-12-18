d7bd3a6a5343ec3544daf384b9b775d376862576
Luca Formaggia <lformaggia@users.noreply.github.com>
2024-12-18 12:11:06 +0100

    Update particle_test.cpp
    Sime other comments on use of random numbers in c++

--- src/particle_test.cpp
+++ src/particle_test.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 
-
+// you have std::clamp that doas that. Anyway, it is fine. 
 double clampVel(double val) {
   if (val < -20) return -20;
   if (val > 20) return 20;
@@ -23,7 +23,26 @@ void setupParticlesParallel(vector<Particle>& particles, float lowerBound, float
     random_device rd;
     mt19937 seed(rd());
     uniform_real_distribution<> randomGenerator(lowerBound, upperBound);
+    //Error! You are using random distribution wrongly!!! distributions call thake in input the number generated bay an engine! Look at the examples in cppreferefnce
+    // Moreover, engines are stateful, so cannot be shared in a multithread environment. You have a data race
+    /*
+    The reason is that the engine store a value that is updated at any call. SO two treads may ge into confict.
+    you should do something like that:
 
+    Generate a few seeds using seed_seq. You need a different seeds to feed the 4 generators for each thread
+    You can use the random device to initialize seed_seq a few values (4 bastano)
+    auto seq = seed_seq(rd(),rd(),rd(),rd());
+    auto num_seeds=numthreads; (numthread e` il numero di thread che volete usare)
+    std::vector<std::unit32_t> seeds(num_seeds);
+    seq.generate(seeds.begin(),seeds(end));// cosi avete dei seed ben distribuiti per la massima entropia
+    #pragma omp parallel // aprite al sezione parallela
+    auto rank = omp_num_thread(); // beccate il rank del thread (dovete includere omp.h)
+    std::mt19937 engine(seeds(rank)); // one engine per thread
+    #pragma omp for schedule(dynamic, 10) num_threads(numthreads)
+        for (size_t i = 0; i < particles.size(); i++) {
+          particles[i] = Particle(randomGenerator(engine()), randomGenerator(engin()), randomGenerator(engine()), randomGenerator(engine()));
+
+    */
     #pragma omp parallel for schedule(dynamic, 10) num_threads(2)
     for (size_t i = 0; i < particles.size(); i++) {
         particles[i] = Particle(randomGenerator(seed), randomGenerator(seed), randomGenerator(seed), randomGenerator(seed));
